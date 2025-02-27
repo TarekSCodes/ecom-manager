c05e5ad120530a6006355dbdb0143a45feda9cdb
TarekSCodes <144847405+TarekSCodes@users.noreply.github.com>
2025-02-27 09:25:58 +0100

    feat: Enhance supplier view with new labels and update layout for improved usability

--- src/main/java/io/github/tarekscodes/controller/SupplierController.java
+++ src/main/java/io/github/tarekscodes/controller/SupplierController.java
@@ -1,8 +1,14 @@
 package io.github.tarekscodes.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class SupplierController {
+
+    @FXML private Label supplierNumberLabel;
+    @FXML private Label supplierNameLabel;
+    @FXML private Label supplierWebsiteLabel;
+    @FXML private Label supplierStatusLabel;
     
     @FXML
     public void initialize() {
